6dbe015e860027066f6e23bb991984f43a7e3cbc
ShiP51 <57025785+ShiP51@users.noreply.github.com>
2019-10-26 12:28:29 +0530

    ShiP51_Hello.cpp
    A Hello World program in C++. Just a basic one maybe.

---
+++ ShiP51_Hello.cpp
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class Hello 
+{
+	public static void main (String[] args) 
+{
+		System.out.println("Hey There !\nThis is Shivam(https://github.com/ShiP51), and it's great starting off with Hello World. \nHappy Coding!!!");
+	}
+}
